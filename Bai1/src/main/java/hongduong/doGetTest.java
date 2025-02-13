a36a18bd8c71783c7a04d214f3ba35ca07f2a7c2
Anh Hong <samh5813p@gmail.com>
2025-02-13 19:03:17 +0700

    test doGet

---
+++ Bai1/src/main/java/hongduong/doGetTest.java
@@ -0,0 +1,36 @@
+package hongduong;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+/**
+ * Servlet implementation class doGetTest
+ */
+@WebServlet("/doGetTest")
+public class doGetTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public doGetTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter wt = response.getWriter();
+		wt.println("Hello, Toi la Anh Hong");
+	}
+
+}
