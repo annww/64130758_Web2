cd722328cdbfcde5a548012e191bb6cd67f8b3c9
Anh Hong <samh5813p@gmail.com>
2025-03-10 10:22:48 +0700

    update code bai tap truyen du lieu

--- TruyenDuLieuSangView/src/main/java/ntu/hongdta/TruyenDuLieuController.java
+++ TruyenDuLieuSangView/src/main/java/ntu/hongdta/TruyenDuLieuController.java
@@ -1,5 +1,18 @@
 package ntu.hongdta;
 
-public class TruyenDuLieuController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class TruyenDuLieuController {
+	@RequestMapping("/truyendulieu")
+	public String sinhVien(ModelMap model) {
+		model.addAttribute("HoTen","Duong Thi Anh Hong");
+		return "sinhVien";
+		
+	}
+	
 }
