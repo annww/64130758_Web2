56f5c57aca761e2a3e290b420bc739de28fd958e
Anh Hong <samh5813p@gmail.com>
2025-03-17 20:24:13 +0700

    bmi update code asian

--- BMISpringBoot/src/main/java/ntu/hongdta/service/BMIService.java
+++ BMISpringBoot/src/main/java/ntu/hongdta/service/BMIService.java
@@ -12,16 +12,28 @@ public class BMIService {
 		return Math.round(bmi*100.0)/100.0;
 	}
 	
-	public String getBMICategory(double bmi) {
-		if(bmi > 18.5f) {
-			return "Bình thường";
+	public String getBMICategory(double bmi, boolean isAsian) {
+		if (isAsian) {
+			if (bmi < 17.5) {
+                return "Thiếu cân";
+            } else if (bmi < 23) { // <= 22.99 có thể viết gọn
+                return "Bình thường";
+            } else if (bmi < 28) { // <= 27.99 có thể viết gọn
+                return "Thừa cân";
+            } else {
+                return "Béo phì";
+            }
 		}
-		else if(bmi >= 25) {
-			return "Thừa cân";
+		 else { 
+		        if (bmi < 18.5) {
+		            return "Gầy";
+		        } else if (bmi >= 18.5 && bmi < 25) {
+		            return "Bình thường";
+		        } else if (bmi >= 25 && bmi < 30) {
+		            return "Thừa cân";
+		        } else {
+		            return "Béo phì";
+		        }
+		    }
 		}
-		else if (bmi >= 30) {
-			return "Béo phì";
-		}
-		else return "Gầy";
 	}
-}
