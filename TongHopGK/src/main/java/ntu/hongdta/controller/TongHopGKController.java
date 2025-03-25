1ec15620d2810bbcd0492b6a0d72e7be5fd60a89
Anh Hong <samh5813p@gmail.com>
2025-03-25 09:21:55 +0700

    update danh sách sinh viên

--- TongHopGK/src/main/java/ntu/hongdta/controller/TongHopGKController.java
+++ TongHopGK/src/main/java/ntu/hongdta/controller/TongHopGKController.java
@@ -1,9 +1,16 @@
 package ntu.hongdta.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.ui.Model;
+import ntu.hongdta.model.SinhVien;
 
 
 @Controller
@@ -19,7 +26,14 @@ public class TongHopGKController {
 	}
 	
 	@GetMapping("/list")
-	public String list() {
+	public String list(Model model) {
+		List<SinhVien> sinhViens = new ArrayList<>();
+		sinhViens.add(new SinhVien("SV1","Dương Thị Ánh Hồng",20,"CNTT"));
+		sinhViens.add(new SinhVien("SV2", "Vi An", 19, "Kinh tế"));
+		sinhViens.add(new SinhVien("SV3", "Mộc Miên", 19, "Kinh tế"));
+		sinhViens.add(new SinhVien("SV4", "An Tuệ", 18, "Ngân hàng"));
+		
+		model.addAttribute("sinhViens", sinhViens);
 		return "list";
 	}
 	
---
+++ TongHopGK/src/main/java/ntu/hongdta/model/SinhVien.java
@@ -0,0 +1,27 @@
+package ntu.hongdta.model;
+
+public class SinhVien {
+	private String id;
+    private String name;
+    private int age;
+    private String major;
+
+    public SinhVien(String id, String name, int age, String major) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getMajor() { return major; }
+    public void setMajor(String major) { this.major = major; }
+}
