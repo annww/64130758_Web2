b0765baa601981e9120c4bd6f001d1b7487a7bbd
Anh Hong <samh5813p@gmail.com>
2025-03-25 09:11:14 +0700

    update view about và home

--- TongHopGK/src/main/java/ntu/hongdta/controller/TongHopGKController.java
+++ TongHopGK/src/main/java/ntu/hongdta/controller/TongHopGKController.java
@@ -1,5 +1,32 @@
 package ntu.hongdta.controller;
 
-public class TongHopGKController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Controller
+public class TongHopGKController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	@GetMapping("/list")
+	public String list() {
+		return "list";
+	}
+	
+	@GetMapping("/addNew")
+	public String addNew() {
+		return "addNew";
+	}
+
+	
 }
