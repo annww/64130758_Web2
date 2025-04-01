40866fa656322b05f9fc13699f7bced7311d7df7
Anh Hong <samh5813p@gmail.com>
2025-04-01 09:10:49 +0700

    update model

--- DuongThiAnhHong_ThiGiuaKy/src/main/java/thiGK/ntu64130758/model/TopicModel.java
+++ DuongThiAnhHong_ThiGiuaKy/src/main/java/thiGK/ntu64130758/model/TopicModel.java
@@ -1,5 +1,40 @@
 package thiGK.ntu64130758.model;
 
 public class TopicModel {
+    private String id;
+    private String name;
+    private String groupID;
+
+    public TopicModel() {}
+
+    public TopicModel(String id, String name, String groupID) {
+        this.id = id;
+        this.name = name;
+        this.groupID = groupID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(String groupID) {
+        this.groupID = groupID;
+    }
 
 }
