58453279384df2ea473c227fb8844693138c97e0
Anh Hong <samh5813p@gmail.com>
2025-04-01 09:01:16 +0700

    tạo dự án thi giữa kỳ

---
+++ DuongThiAnhHong_ThiGK/src/main/java/ntu/hongdta/DuongThiAnhHongThiGkApplication.java
@@ -0,0 +1,13 @@
+package ntu.hongdta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuongThiAnhHongThiGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuongThiAnhHongThiGkApplication.class, args);
+	}
+
+}
